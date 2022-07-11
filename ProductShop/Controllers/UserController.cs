292779c87a63356c4fb9ae812f5c9fb3a422b006
Xam Max <maximcheb21@gmail.com>
2022-07-11 17:17:13 +0300

    После ребилда Баг ушел?!

---
+++ ProductShop/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductShop.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize("AdminRights")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await Task.Run(() => _userManager.Users.ToList()); 
+            if (users!=null)
+            {
+                return View(users);
+            }
+            return View(null);
+        }
+    }
+}
